ccbdf9f95e8d0a249ad775d474a4e880aaaa59c5
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-26 13:31:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  return argument_hash = {
-    key => value
-  }
+  return argument_hash = Hash[key => value]
 end
 
 def read_from_hash(hash, key)
