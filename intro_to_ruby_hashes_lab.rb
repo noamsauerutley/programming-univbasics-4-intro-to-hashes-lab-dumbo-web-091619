e670cdbdfec58b7f53280c7a5e2b74e47ce34b5d
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-26 13:22:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,6 +5,11 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  return hawk_hash = {
+    :hawkguy => "Clint Barton",
+    :hawkeye => "Kate Bishop",
+    :luckey => "Pizza Dog"
+  }
 end
 
 def pioneer
